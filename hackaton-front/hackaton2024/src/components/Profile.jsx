dfd9e5a14d70b392c5ba5158b50d57e7cedfea5c
pinK0der <obsidianfury64@gmail.com>
2024-11-09 10:01:27 +0100

    profile info

--- hackaton-front/hackaton2024/src/components/Profile.jsx
+++ hackaton-front/hackaton2024/src/components/Profile.jsx
@@ -1,10 +1,43 @@
 import "../aditional_css/Profile.css"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import trash from "../assets/trash-can.png"
 import edit from "../assets/edit.png"
 
 function Profile(props)
 {
+    const [infoUser, setInfoUser] = useState(null);
+
+    const getToken = () => {
+        return localStorage.getItem("token");
+    };
+
+    const fetchUser = async () => {
+        const token = getToken(); 
+    
+        const response = await fetch("http://localhost:8088/api/user", {
+          method: "GET",
+          headers: {
+            "User-Agent": "insomnia/10.1.1",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+    
+        if (response.ok) {
+            const data = await response.json();
+            setInfoUser(data)
+        } else {
+          console.error("Failed to fetch user:", response.statusText);
+        }
+      };
+
+    useEffect(() => {
+        fetchUser();  // Викликаємо `fetchUser` при завантаженні компонента
+    }, []);
+
+    if (!infoUser) {
+        return <div>Loading...</div>; // Відображення під час завантаження
+    }
+
     let data = [
         {question: "question1", answer: "answer1"},
         {question: "question2", answer: "answer2"},
@@ -13,16 +46,18 @@ function Profile(props)
         {question: "question5", answer: "answer5"}
     ];
 
+    fetchUser;
+
     return(<>
         <div className="constainer">
             <div className="infoBout">
                 <div className="whatIsInfo"><h2>Profile info</h2></div>
                 <div className="pt">
-                <h2 className="ProfileText">First name: {props.fName}</h2>
-                <h2 className="ProfileText">Last name: {props.lName}</h2>
-                <h2 className="ProfileText">Email: {props.email}</h2>
-                <h2 className="ProfileText">Sex: {props.sex}</h2>
-                <h2 className="ProfileText">BirthDate: {props.birthdate}</h2>
+                <h2 className="ProfileText">First name: {infoUser.firstName}</h2>
+                <h2 className="ProfileText">Last name: {infoUser.lastName}</h2>
+                <h2 className="ProfileText">Email: {infoUser.email}</h2>
+                <h2 className="ProfileText">Sex: {infoUser.sex}</h2>
+                <h2 className="ProfileText">BirthDate: {infoUser.birthDate}</h2>
                 </div>
             </div>
             <div className="zahadkaOtZakaFresko">
