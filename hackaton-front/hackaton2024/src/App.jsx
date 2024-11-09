5e928c8a32c98e3dd38c789fa1cdbe8935a91b77
pinK0der <obsidianfury64@gmail.com>
2024-11-09 09:20:17 +0100

    some ch

--- hackaton-front/hackaton2024/src/App.jsx
+++ hackaton-front/hackaton2024/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Title from './components/Title';
 import Chat from './components/Chat';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         />
         {/* Страница чата */}
         <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
